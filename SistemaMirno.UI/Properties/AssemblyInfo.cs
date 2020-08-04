f03a7bcbd14eb37343eeee4d7b038f4c36b80918
Ihazevich <ihazevich@gmail.com>
2020-08-04 09:58:18 -0400

    New work order UI.

--- SistemaMirno.UI/Properties/AssemblyInfo.cs
+++ SistemaMirno.UI/Properties/AssemblyInfo.cs
@@ -47,6 +47,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.221.20217")]
-[assembly: AssemblyFileVersion("0.1.221.20217")]
-[assembly: AssemblyInformationalVersion("0.1.219")]
+[assembly: AssemblyVersion("0.1.224.20217")]
+[assembly: AssemblyFileVersion("0.1.224.20217")]
+[assembly: AssemblyInformationalVersion("0.1.222")]
