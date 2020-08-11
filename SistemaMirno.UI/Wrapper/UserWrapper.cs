4f8cfbbe493485cc1c59a18c357ae73116e98482
Ihazevich <ihazevich@gmail.com>
2020-08-11 17:17:50 -0400

    Added User Wrapper

---
+++ SistemaMirno.UI/Wrapper/UserWrapper.cs
@@ -0,0 +1,72 @@
+﻿// <copyright file="UserWrapper.cs" company="HazeLabs">
+// Copyright (c) HazeLabs. All rights reserved.
+// </copyright>
+
+using System.Collections.Generic;
+using SistemaMirno.Model;
+
+namespace SistemaMirno.UI.Wrapper
+{
+    /// <summary>
+    /// A class representing the wrapper for the User class.
+    /// </summary>
+    public class UserWrapper : ModelWrapper<User>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserWrapper"/> class.
+        /// </summary>
+        /// <param name="model">The model.</param>
+        public UserWrapper(User model)
+            : base(model)
+        {
+        }
+
+        /// <summary>
+        /// Gets the User ID.
+        /// </summary>
+        public int Id { get { return GetValue<int>(); } }
+
+        /// <summary>
+        /// Gets or sets the User name.
+        /// </summary>
+        public string Name
+        {
+            get { return GetValue<string>(); }
+            set { SetValue(value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the User Access Level.
+        /// </summary>
+        public int AccessLevel
+        {
+            get { return GetValue<int>(); }
+            set { SetValue(value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the User password.
+        /// </summary>
+        public string Password
+        {
+            get { return GetValue<string>(); }
+            set { SetValue(value); }
+        }
+
+        /// <inheritdoc/>
+        protected override IEnumerable<string> ValidateProperty(string propertyName)
+        {
+            switch (propertyName)
+            {
+                case nameof(Name):
+                    if (Name.Length < 4)
+                    {
+                        yield return "El nombre es muy corto.";
+                    }
+
+                    break;
+            }
+        }
+    }
+}
+}
