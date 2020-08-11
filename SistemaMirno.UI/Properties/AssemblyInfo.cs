fc8a3d9732c11e16a5df598e67135ab3e55cadd0
Ihazevich <ihazevich@gmail.com>
2020-08-11 17:01:57 -0400

    version change

--- SistemaMirno.UI/Properties/AssemblyInfo.cs
+++ SistemaMirno.UI/Properties/AssemblyInfo.cs
@@ -47,6 +47,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.38.20223")]
-[assembly: AssemblyFileVersion("0.2.38.20217")]
-[assembly: AssemblyInformationalVersion("0.2.38")]
+[assembly: AssemblyVersion("0.2.39.20224")]
+[assembly: AssemblyFileVersion("0.2.39.20217")]
+[assembly: AssemblyInformationalVersion("0.2.39")]
