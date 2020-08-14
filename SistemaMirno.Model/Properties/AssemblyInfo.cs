5203412750d76c2ffbe5b068f0178beff918ece1
Ihazevich <ihazevich@gmail.com>
2020-08-14 11:01:19 -0400

    Added view for report of units in process by areas.

--- SistemaMirno.Model/Properties/AssemblyInfo.cs
+++ SistemaMirno.Model/Properties/AssemblyInfo.cs
@@ -35,6 +35,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.13.20227")]
-[assembly: AssemblyFileVersion("1.1.13.20217")]
-[assembly: AssemblyInformationalVersion("1.1.13")]
+[assembly: AssemblyVersion("1.1.14.20227")]
+[assembly: AssemblyFileVersion("1.1.14.20217")]
+[assembly: AssemblyInformationalVersion("1.1.14")]
