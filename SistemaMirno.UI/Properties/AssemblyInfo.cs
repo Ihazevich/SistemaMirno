60c7d5395a05cfa8019c74befe68a76696fa8aad
Ihazevich <ihazevich@gmail.com>
2020-08-17 06:21:43 -0400

    Removed unused menu item.

--- SistemaMirno.UI/Properties/AssemblyInfo.cs
+++ SistemaMirno.UI/Properties/AssemblyInfo.cs
@@ -47,6 +47,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.102.20230")]
-[assembly: AssemblyFileVersion("0.2.102.20217")]
-[assembly: AssemblyInformationalVersion("0.2.102")]
+[assembly: AssemblyVersion("0.2.103.20230")]
+[assembly: AssemblyFileVersion("0.2.103.20217")]
+[assembly: AssemblyInformationalVersion("0.2.103")]
