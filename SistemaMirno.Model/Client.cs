7ae983d3f468f1402345a03870ed9c7e0756f214
Ihazevich <ihazevich@gmail.com>
2020-08-18 08:39:37 -0400

    Added Client model

---
+++ SistemaMirno.Model/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaMirno.Model
+{
+    public class Client : BaseModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string RUC { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Department { get; set; }
+    }
+}
--- SistemaMirno.Model/Properties/AssemblyInfo.cs
+++ SistemaMirno.Model/Properties/AssemblyInfo.cs
@@ -35,6 +35,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.24.20231")]
-[assembly: AssemblyFileVersion("1.1.24.20217")]
-[assembly: AssemblyInformationalVersion("1.1.24")]
+[assembly: AssemblyVersion("1.1.25.7535")]
+[assembly: AssemblyFileVersion("1.1.25.7535")]
+[assembly: AssemblyInformationalVersion("1.1.25")]
