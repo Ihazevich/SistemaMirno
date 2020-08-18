e0f7ca29ec4d29be3e950e889426bd98a44ada39
Ihazevich <ihazevich@gmail.com>
2020-08-18 17:16:03 -0400

    Added sale repository and wrapper

---
+++ SistemaMirno.UI/Data/Repositories/ISaleRepository.cs
@@ -0,0 +1,12 @@
+﻿// <copyright file="ISaleRepository.cs" company="HazeLabs">
+// Copyright (c) HazeLabs. All rights reserved.
+// </copyright>
+
+using SistemaMirno.Model;
+
+namespace SistemaMirno.UI.Data.Repositories
+{
+    public interface ISaleRepository : IGenericRepository<Sale>
+    {
+    }
+}
---
+++ SistemaMirno.UI/Data/Repositories/SaleRepository.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="SaleRepository.cs" company="HazeLabs">
+// Copyright (c) HazeLabs. All rights reserved.
+// </copyright>
+
+using SistemaMirno.DataAccess;
+using SistemaMirno.Model;
+
+namespace SistemaMirno.UI.Data.Repositories
+{
+    public class SaleRepository : GenericRepository<Sale, MirnoDbContext>, ISaleRepository
+    {
+        public SaleRepository(MirnoDbContext context)
+            : base(context)
+        {
+        }
+    }
+}
