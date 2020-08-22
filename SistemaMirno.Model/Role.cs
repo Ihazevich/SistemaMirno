cc2e85179f89e7619243fed3b2984523afc1be8f
Ihazevich <ihazevich@gmail.com>
2020-08-22 17:39:43 -0400

    Modified Role model.

--- SistemaMirno.Model/Role.cs
+++ SistemaMirno.Model/Role.cs
@@ -17,24 +17,31 @@ namespace SistemaMirno.Model
         /// Gets or sets the name of the role.
         /// </summary>
         [Required]
-        [MinLength(4)]
+        [StringLength(100)]
         public string Name { get; set; }
 
-        /// <summary>
-        /// Gets or sets the collection of employees that have been assigned to this role.
-        /// </summary>
-        public virtual Collection<Employee> Employees { get; set; }
+        [Required]
+        public int BranchId { get; set; }
 
-        /// <summary>
-        /// Gets or sets the collection of Work Areas that have this role assigned as the responsible role of that Work Area.
-        /// </summary>
-        [InverseProperty("WorkAreaResponsibleRole")]
-        public virtual Collection<WorkArea> WorkAreasResponsibles { get; set; }
+        [ForeignKey(nameof(BranchId))]
+        public Branch Branch { get; set; }
 
-        /// <summary>
-        /// Gets or sets the collection of Work Areas that have this role assigned as the supervisor role of that Work Area.
-        /// </summary>
-        [InverseProperty("WorkAreaSupervisorRole")]
-        public virtual Collection<WorkArea> WorkAreasSupervisors { get; set; }
+        [Required]
+        public bool HasAccessToSales { get; set; }
+
+        [Required]
+        public bool HasAccessToProduction { get; set; }
+
+        [Required]
+        public bool HasAccessToHumanResources { get; set; }
+
+        [Required]
+        public bool HasAccessToAccounting { get; set; }
+
+        [Required]
+        public bool HasAccessToLogistics { get; set; }
+
+        [Required]
+        public bool IsSystemAdmin { get; set; }
     }
 }
