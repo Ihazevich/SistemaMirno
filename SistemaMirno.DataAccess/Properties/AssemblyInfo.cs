6994cece7b3ea6ca473e2576e2a1ce124f2677ba
Ihazevich <ihazevich@gmail.com>
2020-08-21 21:03:07 -0400

    new ndepend report

--- SistemaMirno.DataAccess/Properties/AssemblyInfo.cs
+++ SistemaMirno.DataAccess/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.64.7538")]
-[assembly: AssemblyFileVersion("1.1.64.7538")]
-[assembly: AssemblyInformationalVersion("1.1.64")]
+[assembly: AssemblyVersion("1.1.66.7538")]
+[assembly: AssemblyFileVersion("1.1.66.7538")]
+[assembly: AssemblyInformationalVersion("1.1.66")]
--- SistemaMirno.UI/Properties/AssemblyInfo.cs
+++ SistemaMirno.UI/Properties/AssemblyInfo.cs
@@ -47,6 +47,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.294.7538")]
-[assembly: AssemblyFileVersion("0.2.294.7538")]
-[assembly: AssemblyInformationalVersion("0.2.294")]
+[assembly: AssemblyVersion("0.2.296.7538")]
+[assembly: AssemblyFileVersion("0.2.296.7538")]
+[assembly: AssemblyInformationalVersion("0.2.296")]
