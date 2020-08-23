5765cdf69fde1dae158240ea9f620cdf492d2468
Ihazevich <ihazevich@gmail.com>
2020-08-23 00:52:02 -0400

    Added Delivery related models

---
+++ SistemaMirno.Model/DeliveryUnit.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaMirno.Model
+{
+    public partial class DeliveryUnit : ModelBase
+    {
+        [Required]
+        public int DeliveryId { get; set; }
+
+        [ForeignKey(nameof(DeliveryId))]
+        public virtual Delivery Delivery { get; set; }
+
+        [Required]
+        public int WorkUnitId { get; set; }
+
+        [ForeignKey(nameof(WorkUnitId))]
+        public virtual WorkUnit WorkUnit { get; set; }
+    }
+}
