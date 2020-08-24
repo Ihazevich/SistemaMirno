410bfc2f8bfeecdbe33c0dcbab930e70bf41eca1
Ihazevich <ihazevich@gmail.com>
2020-08-23 22:20:36 -0400

    Added Vehicle model

---
+++ SistemaMirno.Model/Vehicle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaMirno.Model
+{
+    public partial class Vehicle : ModelBase
+    {
+        [Required]
+        public string Model { get; set; }
+
+        [Required]
+        public int Year { get; set; }
+    }
+}
