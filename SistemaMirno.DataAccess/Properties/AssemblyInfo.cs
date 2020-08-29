99818fe84feb28bec81002b88e068195c2d1bdbc
SnekMD <ihazevich@gmail.com>
2020-08-29 09:56:31 -0400

    UI changes

--- SistemaMirno.DataAccess/Properties/AssemblyInfo.cs
+++ SistemaMirno.DataAccess/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.176.7546")]
-[assembly: AssemblyFileVersion("0.2.176.7546")]
-[assembly: AssemblyInformationalVersion("0.2.58")]
+[assembly: AssemblyVersion("0.2.177.7546")]
+[assembly: AssemblyFileVersion("0.2.177.7546")]
+[assembly: AssemblyInformationalVersion("0.2.59")]
