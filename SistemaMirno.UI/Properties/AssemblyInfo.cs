6b67e22511e4e7ad07544e06af0326686388654b
Ihazevich <ihazevich@gmail.com>
2020-09-07 13:14:21 -0400

    Added Prodcution value in work unit area view

--- SistemaMirno.UI/Properties/AssemblyInfo.cs
+++ SistemaMirno.UI/Properties/AssemblyInfo.cs
@@ -47,6 +47,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.1024.7555")]
-[assembly: AssemblyFileVersion("0.3.1024.7555")]
-[assembly: AssemblyInformationalVersion("0.3.1024")]
+[assembly: AssemblyVersion("0.3.1025.7555")]
+[assembly: AssemblyFileVersion("0.3.1025.7555")]
+[assembly: AssemblyInformationalVersion("0.3.1025")]
