b8dd3da70cb38bfd43c40e1bf51533a64375bad4
Ihazevich <ihazevich@gmail.com>
2020-09-15 17:22:05 -0400

    Last free ndpend analysis :C

--- SistemaMirno.UITests/Properties/AssemblyInfo.cs
+++ SistemaMirno.UITests/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.41.20257")]
-[assembly: AssemblyFileVersion("1.0.41.20257")]
+[assembly: AssemblyVersion("1.0.42.20258")]
+[assembly: AssemblyFileVersion("1.0.42.20258")]
 
-[assembly: AssemblyInformationalVersion("1.0.41")]
+[assembly: AssemblyInformationalVersion("1.0.42")]
